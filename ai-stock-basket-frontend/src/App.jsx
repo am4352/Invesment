72b4f6f77e95553ad3cf63b33102f1d2299adbda
am4352 <am4352@srmist.edu.in>
2024-10-10 10:07:10 +0530

    changed

--- ai-stock-basket-frontend/src/App.jsx
+++ ai-stock-basket-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import SentimentChecker from "./Pages/Sentiment";
 import Recommendations from "./Pages/Recommendations";
 import HomePage from "./Pages/Home";
+import AboutUs from "./Pages/AboutUs"; // Import the About Us page component
 import './App.css'; // Make sure to import your CSS file
 
 function App() {
@@ -21,6 +22,9 @@ function App() {
             <li>
               <Link to="/recommendations">Stock Recommendations</Link>
             </li>
+            <li>
+              <Link to="/about">About Us</Link> {/* Add the About Us link */}
+            </li>
           </ul>
         </nav>
 
@@ -29,6 +33,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/sentiment" element={<SentimentChecker />} />
           <Route path="/recommendations" element={<Recommendations />} />
+          <Route path="/about" element={<AboutUs />} /> {/* Define the About Us route */}
         </Routes>
       </div>
     </Router>
